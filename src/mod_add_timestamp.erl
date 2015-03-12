b764f5995550e96a487071c62711e712a67805df
Taras Repa <trepa@malkosua.com>
2015-03-12 16:41:08 +0200

    Make module working for newer ejabberd versions

--- src/mod_add_timestamp.erl
+++ src/mod_add_timestamp.erl
@@ -1,7 +1,7 @@
 %% name of module must match file name
 -module(mod_add_timestamp).
  
--author("Johan Vorster").
+-author("trepa").
  
 %% Every ejabberd module implements the gen_mod behavior
 %% The gen_mod behavior requires two functions: start/2 and stop/1
@@ -12,33 +12,33 @@
  
 %% included for writing to ejabberd log file
 -include("ejabberd.hrl").
+-include("logger.hrl").
+-include("jlib.hrl").
 
 start(_Host, _Opt) -> 
-    ?INFO_MSG("starting mod_add_timestamp", []),
+    error_logger:info_msg("starting mod_add_timestamp", []),
     ejabberd_hooks:add(filter_packet, global, ?MODULE, on_filter_packet, 120).
 
 stop(_Host) -> 
-    ?INFO_MSG("stopping mod_add_timestamp", []),
+    error_logger:info_msg("stopping mod_add_timestamp", []),
     ejabberd_hooks:delete(filter_packet, global, ?MODULE, on_filter_packet, 120).
 
-on_filter_packet({From, To, XML} = Packet) ->
-    Type = xml:get_tag_attr_s("type", XML),
-    DataTag = xml:get_subtag(XML, "data"), 
-    %% Add timestamp to chat message and where no DataTag exist 
-    case Type =:= "groupchat" andalso DataTag =:= false of
-        true -> 
-            Timestamp = now_to_microseconds(erlang:now()),
-            FlatTimeStamp = lists:flatten(io_lib:format("~p", [Timestamp])),
-            XMLTag = {xmlelement,"data", [{"timestamp", FlatTimeStamp}], []}, 
-            TimeStampedPacket = xml:append_subtags(XML, [XMLTag]),
-            ReturnPacket = {From, To, TimeStampedPacket},
-            Return = ReturnPacket;
+on_filter_packet({ _, _, #xmlel{name = <<"message">>, attrs = [{<<"type">>,<<"normal">>}], children = _} } = Packet) -> Packet;
+on_filter_packet({ From, To, #xmlel{name = <<"message">>, attrs = _Attrs, children = _SubEl} = Xml} = Packet) ->
+    case xml:get_subtag(Xml, <<"data">>) of
         false ->
-            Return = Packet
-    end,
-    Return.
+            Timestamp = get_timestamp(),
+            XMLTag = #xmlel{ name = <<"data">>, attrs = [{<<"timestamp">>, list_to_binary([Timestamp])}]},
+            NewXml = xml:append_subtags(Xml, [XMLTag]),
+            NewPacket = {From, To, NewXml},
+            NewPacket;
+        _ ->
+            Packet
+    end;
     
-now_to_microseconds({Mega, Sec, Micro}) ->
-    %%Epoch time in milliseconds from 1 Jan 1970
-    (Mega*1000000 + Sec)*1000000 + Micro. 
+on_filter_packet(FullPacket) -> FullPacket.
+    
+get_timestamp()->
+    {Mega, Secs, Micro} = now(),
+    io_lib:format("~p", [(Mega*1000000 + Secs)*1000000 + Micro]).
 
